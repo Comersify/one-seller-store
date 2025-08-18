c67ca40293dbb9651a6ebb4f790664d8bd863fbe
saadaoui-salah <salahsaadaoui8@gmail.com>
2025-08-18 05:22:48 +0100

    update

--- app/planetgames/products/[slug]/page.js
+++ app/planetgames/products/[slug]/page.js
@@ -11,7 +11,7 @@ import React from "react";
  * Props:
  *  - slides: Array<{ src: string, alt?: string, caption?: string }>
  */
-export function TailwindSlider({
+function TailwindSlider({
   slides,
 }) {
   const name = "twslider-" + Math.random().toString(36).slice(2, 7);
